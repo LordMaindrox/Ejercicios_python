6d92c49bc5407190fc82d072168dca36773edc8b
Albert Escobar <albertescobar@Alberts-MacBook-Air.local>
2024-07-11 23:37:02 -0600

    ciclo_while

---
+++ ejercicios_curso/ciclo_while.py
@@ -0,0 +1,5 @@
+numero = int(input("ingrese un número menor de 10: "))
+while numero > 10:
+  print("Incorrecto, escriba un número menor de 10")
+  numero = int(input("ingrese un número menor de 10: "))
+print(numero)
