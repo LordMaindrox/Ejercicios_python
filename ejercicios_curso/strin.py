ce6a4fd32a3c13da49cd89379138c25545ee41c1
Albert Escobar <maindrox446@gmail.com>
2024-07-15 22:37:55 -0600

    metodos_array

---
+++ ejercicios_curso/strin.py
@@ -0,0 +1,10 @@
+def run():
+    name = 'MAINDROX'
+    print(name.count('X'))
+    print(name.capitalize())
+    print(name.title())
+    print(name.swapcase())
+
+if __name__ ==  '__main__':
+    run()   
+
