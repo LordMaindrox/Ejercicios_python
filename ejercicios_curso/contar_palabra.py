e13f795c8dfe65b880ea796b414aa0e49386bc78
Albert Escobar <maindrox446@gmail.com>
2024-07-23 23:49:48 -0600

    contar palabras

---
+++ ejercicios_curso/contar_palabra.py
@@ -0,0 +1,18 @@
+cadena = "hola mundo hola mundo mundo JESSICA"
+contar = 0
+
+recuento = []
+diccionario = {}
+for palabra in cadena.split():
+    #contar = contar +1
+    recuento.append(palabra)
+    #print(palabra)
+#print(contar)
+#print(recuento)
+for i in recuento:
+    marca = 0
+    for j in recuento:
+        if i ==j:
+            marca = marca+1
+        diccionario[i] = marca
+print(diccionario)
