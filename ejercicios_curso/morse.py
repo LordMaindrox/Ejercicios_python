58aa51d3fd7c94e9504278a3415f56008068aaeb
Albert Escobar <maindrox446@gmail.com>
2024-07-27 00:44:55 -0600

    morse2

--- ejercicios_curso/morse.py
+++ ejercicios_curso/morse.py
@@ -8,13 +8,53 @@ morse_code = {
     '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
     '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----',
     ', ': '--..--', '.': '.-.-.-', '?': '..--..', '/': '-..-.', '-': '-....-', 
-    '(': '-.--.',  ')': '-.--.-'}
+    '(': '-.--.',  ')': '-.--.-', ' ': ' '}
 
-
-frase = 'SOS'
+morse_inverso = {
+    '.-': 'A',    '-...': 'B',  '-.-.': 'C',  '-..': 'D',   '.': 'E',
+    '..-.': 'F',  '--.': 'G',   '....': 'H',  '..': 'I',    '.---': 'J',
+    '-.-': 'K',   '.-..': 'L',  '--': 'M',    '-.': 'N',    '---': 'O',
+    '.--.': 'P',  '--.-': 'Q',  '.-.': 'R',   '...': 'S',   '-': 'T',
+    '..-': 'U',   '...-': 'V',  '.--': 'W',   '-..-': 'X',  '-.--': 'Y',
+    '--..': 'Z', 
+    '.----': '1', '..---': '2', '...--': '3', '....-': '4', '.....': '5',
+    '-....': '6', '--...': '7', '---..': '8', '----.': '9', '-----': '0',
+    '--..--': ',', '.-.-.-': '.', '..--..': '?', '-..-.': '/', '-....-': '-',
+    '-.--.': '(', '-.--.-': ')', ' ': ' '
+}
 
 def conversorMorse (palabra):
     list(palabra)
     for letra in palabra:
         print(morse_code[letra], end=" ")
-conversorMorse(frase)
+
+def conversorAlpha(palabra):
+    lista=palabra.split(',')
+    for letra in lista:
+        print(morse_inverso[letra], end=" ")
+
+def esNumero(valor):
+    try:
+        numero = int(valor)
+        return True
+    except ValueError:
+        return False
+
+print(""" Seleccione lo que desea hacer:  
+      1.- De Alfanumérico a Morse
+      2.- De Morse a Alfanumérico """)
+seleccion = input(" Respuesta:  ")
+
+
+if esNumero(seleccion):
+    if seleccion == '1' or seleccion == '2':
+        if seleccion == '1':
+            frase = input("Ingrese una palabra ")
+            conversorMorse(frase.upper())
+        elif seleccion == '2': 
+            frase = input("Ingrese código separado por comas \",\" : ")
+            conversorAlpha(frase)
+    else: 
+        print("Selección incorrecta")
+else: 
+    print("Valor incorrecto")
